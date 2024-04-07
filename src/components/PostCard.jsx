9cf84b7367b7e78d33ad4c6de21315ab5fbc1336
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-04-07 21:15:52 +0530

    card designed

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({$id,title,featuredImage}) {
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
                 <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
+                className='rounded-xl h-60 w-60 m-auto' />
 
             </div>
             <h2
